fd2059ccd82a238e92f344d9647f3cc4ffaf6be7
rashdan182011 <78985293+rashdan182011@users.noreply.github.com>
2021-09-06 09:45:08 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+ import * as firebase from "firebase";
+
+//initiaconst firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyCbq6xZHuhslIQvNRYLggvyHFrfX2KkFyI",
+  authDomain: "project-60-c9572.firebaseapp.com",
+  projectId: "project-60-c9572",
+  storageBucket: "project-60-c9572.appspot.com",
+  messagingSenderId: "493920755844",
+  appId: "1:493920755844:web:8e654960a779d50f8262d1"
+};
+
+  export default firebase.database()
+ 
+
+  
